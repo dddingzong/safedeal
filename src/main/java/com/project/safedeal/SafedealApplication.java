427c2a69e8b2dbd0ae7a3256e65182f1fb8a40ac
USER <cji513@naver.com>
2025-05-23 11:34:12 +0900

    Initial commit

---
+++ src/main/java/com/project/safedeal/SafedealApplication.java
@@ -0,0 +1,13 @@
+package com.project.safedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafedealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafedealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/safedeal/SafedealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.safedeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafedealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
